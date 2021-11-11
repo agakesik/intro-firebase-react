1a97bb795db873f71201716907446276454e3e0d
agakesik <aga.k396@gmail.com>
2021-11-11 16:00:15 +0100

    Code from tutoriol, updated for current tooles, with hosting on firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "firebase/compat/app";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBUwzRQxNq5p7xqekp3wMmHNXtjz_nuDWE",
+  authDomain: "intro-firebase-react-6ccdd.firebaseapp.com",
+  projectId: "intro-firebase-react-6ccdd",
+  storageBucket: "intro-firebase-react-6ccdd.appspot.com",
+  messagingSenderId: "225674993936",
+  appId: "1:225674993936:web:65abf56600183ffe754595",
+});
 
 function App() {
+  const [name, setName] = useState("");
+  const [currentItem, setCurrentItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  useEffect(async () => {
+    const db = getFirestore();
+    const dbItems = await getDocs(collection(db, "items"));
+    let newItems = [];
+    dbItems.forEach((doc) => newItems.push(doc.data()));
+    setItems(newItems);
+  }, []);
+
+  const handleChange = (e) => {
+    if (e.target.name === "name") {
+      setName(e.target.value);
+    } else {
+      setCurrentItem(e.target.value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const db = getFirestore();
+
+    const docRef = await addDoc(collection(db, "items"), {
+      name: name,
+      item: currentItem,
+    });
+    console.log("Document written with ID: ", docRef.id);
+
+    setName("");
+    setCurrentItem("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <div className="wrapper">
+          <h1>Fun Food Friends</h1>
+        </div>
       </header>
+      <div className="container">
+        <section className="add-item">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="name"
+              placeholder="What's your name?"
+              onChange={handleChange}
+              value={name}
+            />
+            <input
+              type="text"
+              name="currentItem"
+              placeholder="What are you bringing?"
+              onChange={handleChange}
+              value={currentItem}
+            />
+            <button>Add Item</button>
+          </form>
+        </section>
+        <section className="display-item">
+          <div className="wrapper">
+            <ul>
+              {items.map((item) => {
+                return (
+                  <li>
+                    <h3>{item.item}</h3>
+                    <p>brought by: {item.name}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
-
 export default App;
